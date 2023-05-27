1215d93b06ca5b0096bc5c9d9b802a609ef5a08d
Funxy <jaakko.kakko97@gmail.com>
2023-05-27 06:27:27 +0300

    Add imageviewer with drag-scroll and zoom + some other front end

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import './App.css';
-import handleSubmit from './handles/handlesubmit';
-import { useRef } from 'react';
-import resetDatabase from './resetDatabase';
+import './Styles/App.css';
+import { useState } from 'react';
+import Header from './Components/Header'
+import MainView from './Components/MainView'
+import Footer from './Components/Footer'
 
 
 function App() {
-  const dataRef = useRef()
-
-  const submithandler = (e) => {
-    e.preventDefault();
-    const value = dataRef.current.value;
-    const strData = value.split(",");
-    // [id,x,y]
-    // for now the id's are 0-9
-    const intData = [
-      parseInt(strData[0]),
-      parseInt(strData[1]),
-      parseInt(strData[2])
-    ];
-    try {
-      handleSubmit(intData)
-    } catch (error) {
-      console.log(error);
-    }
-    dataRef.current.value = ""
-  }
-
+  // 0: pre-game, 1: game, 2: post-game
+  const [gameState, setGameState] = useState(0);
 
   return (
-    <div className="App">
-      <form onSubmit={submithandler}>
-        <input type="text" ref={dataRef} />
-        <button type="submit">Save</button>
-      </form>
-      <button onClick={resetDatabase}>reset</button>
+    <div id="App">
+      <Header/>
+      <MainView/>
+      <Footer/>
     </div>
   );
 }
