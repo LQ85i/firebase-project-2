f06ced954d1618919a17c6f69c00b1ec9028e415
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-06-20 21:22:41 +0300

    Update backend code

--- src/resetDatabase.js
+++ src/resetDatabase.js
@@ -1,8 +1,4 @@
-import { collection, getDocs, updateDoc } from "@firebase/firestore"
-import { firestore } from "./firebase_setup/firebase"
-
-
-function generateRandomIndices() {
+function getActiveCharacters() {
     let indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let randomIndices = [];
 
@@ -17,33 +13,6 @@ function generateRandomIndices() {
     // Get the first three indices as random indices
     randomIndices = indices.slice(0, 3);
     return randomIndices;
-}
-
-
-function randomizeCharacters() {
-    let collectionRef = collection(firestore, "active_characters");
-
-    // Generate random indices
-    // For having 3 random active characters out of 10
-    let randomIndices = generateRandomIndices();
-    // Get all documents in the collection
-    // and update isActive according to random indices
-    getDocs(collectionRef).then(function (querySnapshot) {
-        let index = 0;
-        querySnapshot.forEach(function (doc) {
-            let isActive = randomIndices.includes(index);
-            updateDoc(doc.ref, { isActive: isActive });
-            index++;
-        });
-    }).catch(function (error) {
-        console.error("Error writing to database: ", error);
-    });
-    return randomIndices;
-}
-
-function getActiveCharacters() {
-    const IDs = randomizeCharacters();
-    return IDs;
 }
 
 export default getActiveCharacters;
