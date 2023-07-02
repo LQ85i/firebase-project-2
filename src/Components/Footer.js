2877974dd8f57fe3436d671e603ee8f7f84b51b7
LQ85i <39389364+LQ85i@users.noreply.github.com>
2023-07-02 04:19:26 +0300

    Update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import '../Styles/Footer.css';
 
 const Footer = (props) => {
     return ( <div id="footer">
-        <div className='left'>Website by <a href="https://github.com/LQ85i">LQ85i</a></div>
+        <div className='left'>Website by <a href="https://github.com/LQ85i/firebase-project-2">LQ85i</a></div>
         <div className='right'>Artwork by <a href="https://www.instagram.com/oscarsun.art/">OscarSun.Art</a></div>
     </div> );
 }
